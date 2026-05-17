98b6f94c4dd4235f2d3e5b8ff3373eaa6946aee0
JCARNEIROX <j239738@dac.unicamp.br>
2026-05-17 17:04:09 -0300

    Subtrator e teste

--- Algoritmo_MDC/modules/Subtractor/rtl/Subtractor.v
+++ Algoritmo_MDC/modules/Subtractor/rtl/Subtractor.v
@@ -1,6 +1,15 @@
-module Subtractor (
+
+module Subtractor #(
+  parameter WIDTH = 8
+)(
+  input  wire [WIDTH-1:0] x,
+  input  wire [WIDTH-1:0] y,
+  input  wire Bin,
+  output reg [WIDTH-1:0] D,
+  output reg Bout
 );
-
-    // Sua lógica aqui
-
+  always @(*) begin
+    {Bout, D} = x - y - Bin;
+  end
+  
 endmodule
--- Algoritmo_MDC/modules/Subtractor/tb/Subtractor_tb.v
+++ Algoritmo_MDC/modules/Subtractor/tb/Subtractor_tb.v
@@ -2,19 +2,55 @@
 
 module tb_Subtractor;
 
-    Subtractor dut (
+    parameter WIDTH = 8;
+    reg [WIDTH-1:0] x,y;
+    reg Bin;
+    wire [WIDTH-1:0] D;
+    wire Bout;
+
+    Subtractor #(.WIDTH(WIDTH)) dut 
+    (
+        .x(x),
+        .y(y),
+        .Bin(Bin),
+        .D(D),
+        .Bout(Bout)
     );
 
     initial begin
         $dumpfile("Subtractor.vcd");
         $dumpvars(0, tb_Subtractor);
+        $monitor("At time %t: x=%d, y=%d, Bin=%b, D=%b, Bout=%b", 
+                $time, dut.x, dut.y, dut.Bin, dut.D, dut.Bout);
 
         // Inicialização
-        #20 reset = 0;
+        x = 0;
+        y = 0;
+        Bin = 0;
+
+        #10 
+        x = 3;
+        y = 1;
+        Bin = 0; // 3-1-0 = 2
+
+        #10
+        x = 3;
+        y = 1;
+        Bin = 1; // 3-1-1 = 1
+
+        #10
+        x = 1;
+        y = 3;
+        Bin = 0; // 1-3-0 = -2 (Bout=1, D=2)
+
+        #10
+        x = 1;
+        y = 3;
+        Bin = 1; // 1-3-1 = -3 (Bout=1, D=1)
 
         // Estímulos do teste aqui
 
-        #100 $finish;
+        #50 $finish;
     end
 
 endmodule
