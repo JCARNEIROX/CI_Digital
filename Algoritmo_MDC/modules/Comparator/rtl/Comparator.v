c483958d73ce2c4435714fcfe2ab22221b1d32ae
JCARNEIROX <j239738@dac.unicamp.br>
2026-05-17 16:36:23 -0300

    Comparador e teste

--- Algoritmo_MDC/modules/Comparator/rtl/Comparator.v
+++ Algoritmo_MDC/modules/Comparator/rtl/Comparator.v
@@ -1,6 +1,16 @@
-module Comparator (
+module Comparator #(
+    parameter N = 8
+)(
+    input wire [N-1:0] x,
+    input wire [N-1:0] y,
+    output wire x_eq_y,
+    output wire x_gt_y,
+    output wire x_lt_y
 );
 
     // Sua lógica aqui
+    assign x_eq_y = (x == y); // x==y
+    assign x_gt_y = (x > y) ? 1'b1 : 1'b0; // x>y, x_gt_y = 1 e x_lt_y = 0
+    assign x_lt_y = (x < y) ? 1'b1 : 1'b0; // x<y, x_gt_y = 0 e x_lt_y = 1
 
 endmodule
--- Algoritmo_MDC/modules/Comparator/tb/Comparator_tb.v
+++ Algoritmo_MDC/modules/Comparator/tb/Comparator_tb.v
@@ -2,19 +2,45 @@
 
 module tb_Comparator;
 
-    Comparator dut (
+    parameter N = 8;
+    reg [N-1:0] x, y;
+    wire x_eq_y, x_gt_y, x_lt_y;
+
+    Comparator #(
+        .N(N)
+    )dut(
+        .x(x),
+        .y(y),
+        .x_eq_y(x_eq_y),
+        .x_gt_y(x_gt_y),
+        .x_lt_y(x_lt_y)
     );
 
     initial begin
         $dumpfile("Comparator.vcd");
         $dumpvars(0, tb_Comparator);
+        $monitor("Time: %0t | x: %d | y: %d | x_eq_y: %b | x_gt_y: %b | x_lt_y: %b", 
+                $time, x, y, x_eq_y, x_gt_y, x_lt_y);
 
         // Inicialização
-        #20 reset = 0;
+        x = 0;
+        y = 0; // x == y
+
+        #10; // Aguarda 10 ns
+        x = 15;
+        y = 15; // x == y
+        
+        #10; // Aguarda 10 ns
+        x = 5;
+        y = 3; // x > y
+
+        #10; // Aguarda 10 ns
+        x = 2;
+        y = 4; // x < y
 
         // Estímulos do teste aqui
 
-        #100 $finish;
+        #50 $finish;
     end
 
 endmodule
