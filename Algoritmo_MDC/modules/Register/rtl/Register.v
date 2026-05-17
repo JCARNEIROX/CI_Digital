88d5c41f38d94f302887fb22d2b5e685c300a1fa
JCARNEIROX <j239738@dac.unicamp.br>
2026-05-17 16:14:47 -0300

    Modulo Register e test bench

--- Algoritmo_MDC/modules/Register/rtl/Register.v
+++ Algoritmo_MDC/modules/Register/rtl/Register.v
@@ -1,6 +1,24 @@
-module Register (
+// Modulo de registrdor PIPO
+module Register #(
+    parameter N = 8
+)(
+    input wire clk,
+    input wire rst,
+    input wire en,
+    input wire [N-1:0] din,
+    output reg [N-1:0] dout
 );
 
-    // Sua lógica aqui
+    always @(posedge clk) begin
+        if (rst) begin
+            dout <= 0;
+        end else if (en) begin
+            dout <= din;
+        end
+        else begin
+            dout <= dout; // Mantém o valor atual se en não estiver ativo
+        end
+
+    end
 
 endmodule
--- Algoritmo_MDC/modules/Register/tb/Register_tb.v
+++ Algoritmo_MDC/modules/Register/tb/Register_tb.v
@@ -1,20 +1,57 @@
-`timescale 1ns/1ns
+`timescale 1ns/1ps
 
 module tb_Register;
 
-    Register dut (
+    parameter N = 8;
+
+    reg [N-1:0] din;
+    reg clk, rst, en;
+    wire [N-1:0] dout;
+
+    Register #(.N(N)) dut (
+        .clk(clk),
+        .rst(rst),
+        .en(en),
+        .din(din),
+        .dout(dout)
     );
+
+    // Clock de 10 ns
+    always #5 clk = ~clk;
 
     initial begin
         $dumpfile("Register.vcd");
         $dumpvars(0, tb_Register);
 
+        $monitor("Time: %0t | clk: %b | rst: %b | en: %b | din: %d | dout: %d", 
+                 $time, clk, rst, en, din, dout);
+
         // Inicialização
-        #20 reset = 0;
+        clk = 0;
+        rst = 1;   // ativa reset
+        en  = 0;
+        din = 0;
 
-        // Estímulos do teste aqui
+        #10;
+        rst = 0;   // desativa reset
+        en  = 1;
+        din = 5;
 
-        #100 $finish;
+        #10;
+        din = 10;
+
+        #10;
+        en = 0;
+        din = 20;  // dout NÃO deve mudar, pois en = 0
+
+        #10;
+        en = 1;    // dout deve atualizar para 20 no próximo clock
+
+        #10
+        rst = 1;   // ativa reset novamente, dout deve voltar para 0
+
+        #30;
+        $finish;
     end
 
 endmodule
