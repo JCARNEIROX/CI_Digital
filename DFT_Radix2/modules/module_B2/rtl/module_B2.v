ebb67656b16feebb16a0a124018c5bfcba2ceb5d
JCARNEIROX <j239738@dac.unicamp.br>
2026-05-29 09:39:41 -0300

    Correção B2

--- DFT_Radix2/modules/module_B2/rtl/module_B2.v
+++ DFT_Radix2/modules/module_B2/rtl/module_B2.v
@@ -1,117 +1,35 @@
-module module_B4 #(
-    parameter WIDTH = 8,
-    parameter N = 4
+// Code your design here
+module radix2_2In #(
+    parameter integer WIDTH = 8
 )(
-    input  signed [N*WIDTH-1:0] xr_in,
-    input  signed [N*WIDTH-1:0] xi_in,
-
-    output signed [N*WIDTH-1:0] Xr_out,
-    output signed [N*WIDTH-1:0] Xi_out,
-
-    input  signed [N*WIDTH-1:0] Wr_in,
-    input  signed [N*WIDTH-1:0] Wi_in
+    input signed [2*WIDTH-1:0] A, // A value com A[0] real e A[1] imaginário com Width bits
+    input signed [2*WIDTH-1:0] B,  // B value com B[0] real e B[1] imaginário com Width bits
+    input signed [2*WIDTH-1:0] W, // W value com W[0] real e W[1] imaginário com Width bits
+    
+    output signed [2*WIDTH-1:0] X0, // X1 value com X1[0] real e X1[1] imaginário com Width bits
+    output signed [2*WIDTH-1:0] X1 // X2 value com X2[0] real e X2[1] imaginário com Width bits
 );
+    //Sepração dos componentes real e imaginário de A, B e W
+    wire signed [WIDTH-1:0] A_real = A[WIDTH-1:0];
+    wire signed [WIDTH-1:0] A_imag = A[2*WIDTH-1:WIDTH];
 
-    wire signed [WIDTH-1:0] xr [0:N-1];
-    wire signed [WIDTH-1:0] xi [0:N-1];
+    wire signed [WIDTH-1:0] B_real = B[WIDTH-1:0];
+    wire signed [WIDTH-1:0] B_imag = B[2*WIDTH-1:WIDTH];
 
-    wire signed [WIDTH-1:0] wr [0:N-1];
-    wire signed [WIDTH-1:0] wi [0:N-1];
+    wire signed [WIDTH-1:0] W_real = W[WIDTH-1:0];
+    wire signed [WIDTH-1:0] W_imag = W[2*WIDTH-1:WIDTH];
 
-    genvar i;
+    // Variáveis para armazenar os resultados intermediários de B*W
+    wire signed [2*WIDTH-1:0] bw; // bw[0] real e bw[1] imaginário com Width bits cada
 
-    generate
-        for (i = 0; i < N; i = i + 1) begin : input_split
-            assign xr[i] = xr_in[(i+1)*WIDTH-1:i*WIDTH];
-            assign xi[i] = xi_in[(i+1)*WIDTH-1:i*WIDTH];
+    // Cálculo de B*W
+    assign bw[WIDTH-1:0] = (B_real * W_real) - (B_imag * W_imag); // Parte real de B*W
+    assign bw[2*WIDTH-1:WIDTH] = (B_real * W_imag) + (B_imag * W_real); // Parte imaginária de B*W
 
-            assign wr[i] = Wr_in[(i+1)*WIDTH-1:i*WIDTH];
-            assign wi[i] = Wi_in[(i+1)*WIDTH-1:i*WIDTH];
-        end
-    endgenerate
-
-    // Saídas intermediárias da etapa 1
-    wire signed [2*WIDTH-1:0] S0;
-    wire signed [2*WIDTH-1:0] S1;
-    wire signed [2*WIDTH-1:0] S2;
-    wire signed [2*WIDTH-1:0] S3;
-
-    // Saídas finais complexas
-    wire signed [2*WIDTH-1:0] X0_c;
-    wire signed [2*WIDTH-1:0] X1_c;
-    wire signed [2*WIDTH-1:0] X2_c;
-    wire signed [2*WIDTH-1:0] X3_c;
-
-    // ------------------------------------------------------------
-    // Etapa 1
-    // x0 com x2 usando W4^0
-    // x1 com x3 usando W4^0
-    // ------------------------------------------------------------
-
-    radix2_2In #(
-        .WIDTH(WIDTH)
-    ) butterfly1_stage1 (
-        .A  ({xi[0], xr[0]}),
-        .B  ({xi[2], xr[2]}),
-        .W  ({wi[0], wr[0]}),
-        .X0 (S0),
-        .X1 (S1)
-    );
-
-    radix2_2In #(
-        .WIDTH(WIDTH)
-    ) butterfly2_stage1 (
-        .A  ({xi[1], xr[1]}),
-        .B  ({xi[3], xr[3]}),
-        .W  ({wi[0], wr[0]}),
-        .X0 (S2),
-        .X1 (S3)
-    );
-
-    // ------------------------------------------------------------
-    // Etapa 2
-    // S0 com S2 usando W4^0 -> X0 e X2
-    // S1 com S3 usando W4^1 -> X1 e X3
-    // ------------------------------------------------------------
-
-    radix2_2In #(
-        .WIDTH(WIDTH)
-    ) butterfly1_stage2 (
-        .A  (S0),
-        .B  (S2),
-        .W  ({wi[0], wr[0]}),
-        .X0 (X0_c),
-        .X1 (X2_c)
-    );
-
-    radix2_2In #(
-        .WIDTH(WIDTH)
-    ) butterfly2_stage2 (
-        .A  (S1),
-        .B  (S3),
-        .W  ({wi[1], wr[1]}),
-        .X0 (X1_c),
-        .X1 (X3_c)
-    );
-
-    // ------------------------------------------------------------
-    // Separação das saídas finais
-    //
-    // Como o padrão interno é:
-    // [WIDTH-1:0]       = real
-    // [2*WIDTH-1:WIDTH] = imaginário
-    // ------------------------------------------------------------
-
-    assign Xr_out[(0+1)*WIDTH-1:0*WIDTH] = X0_c[WIDTH-1:0];
-    assign Xi_out[(0+1)*WIDTH-1:0*WIDTH] = X0_c[2*WIDTH-1:WIDTH];
-
-    assign Xr_out[(1+1)*WIDTH-1:1*WIDTH] = X1_c[WIDTH-1:0];
-    assign Xi_out[(1+1)*WIDTH-1:1*WIDTH] = X1_c[2*WIDTH-1:WIDTH];
-
-    assign Xr_out[(2+1)*WIDTH-1:2*WIDTH] = X2_c[WIDTH-1:0];
-    assign Xi_out[(2+1)*WIDTH-1:2*WIDTH] = X2_c[2*WIDTH-1:WIDTH];
-
-    assign Xr_out[(3+1)*WIDTH-1:3*WIDTH] = X3_c[WIDTH-1:0];
-    assign Xi_out[(3+1)*WIDTH-1:3*WIDTH] = X3_c[2*WIDTH-1:WIDTH];
+    // Cálculo de X1 e X2
+    assign X0[WIDTH-1:0] = A_real + bw[WIDTH-1:0]; // Parte real de X0
+    assign X0[2*WIDTH-1:WIDTH] = A_imag + bw[2*WIDTH-1:WIDTH]; // Parte imaginária de X0
+    assign X1[WIDTH-1:0] = A_real - bw[WIDTH-1:0]; // Parte real de X1
+    assign X1[2*WIDTH-1:WIDTH] = A_imag - bw[2*WIDTH-1:WIDTH]; // Parte imaginária de X1
 
 endmodule
--- DFT_Radix2/modules/module_B4/tb/module_B4_tb.v
+++ DFT_Radix2/modules/module_B4/tb/module_B4_tb.v
@@ -2,19 +2,69 @@
 
 module tb_module_B4;
 
-    module_B4 dut (
+    parameter WIDTH = 8;
+
+    reg signed [WIDTH-1:0] a_r, a_i;
+    reg signed [WIDTH-1:0] b_r, b_i;
+    reg signed [WIDTH-1:0] w_r, w_i;
+
+    wire signed [WIDTH-1:0] x1_r, x1_i;
+    wire signed [WIDTH-1:0] x2_r, x2_i;
+    wire signed [WIDTH-1:0] bw_r, bw_i;
+
+    wire signed [2*WIDTH-1:0] A;
+    wire signed [2*WIDTH-1:0] B;
+    wire signed [2*WIDTH-1:0] W;
+
+    wire signed [2*WIDTH-1:0] X0;
+    wire signed [2*WIDTH-1:0] X1;
+
+    // Montagem dos valores complexos
+    assign A = {a_i, a_r};
+    assign B = {b_i, b_r};
+    assign W = {w_i, w_r};
+
+    // Separação dos resultados
+    assign x1_r = X0[WIDTH-1:0];
+    assign x1_i = X0[2*WIDTH-1:WIDTH];
+
+    assign x2_r = X1[WIDTH-1:0];
+    assign x2_i = X1[2*WIDTH-1:WIDTH];
+
+    radix2_2In #(
+        .WIDTH(WIDTH)
+    ) dut (
+        .A(A),
+        .B(B),
+        .W(W),
+        .X0(X0),
+        .X1(X1)
     );
 
     initial begin
-        $dumpfile("module_B4.vcd");
-        $dumpvars(0, tb_module_B4);
 
-        // Inicialização
-        #20 reset = 0;
+        // passo 1
+        a_r = 0;
+        a_i = 0;
+        b_r = 2;
+        b_i = 0;
+        w_r = 1;
+        w_i = 0;
 
-        // Estímulos do teste aqui
+        #1;
+        $display("a = (%d + j%d), b = (%d + j%d), w = (%d + j%d) --> x1 = (%d + j%d), x2 = (%d + j%d)",
+                 a_r, a_i, b_r, b_i, w_r, w_i, x1_r, x1_i, x2_r, x2_i);
 
-        #100 $finish;
+
+        #5;
+        $finish;
+
     end
 
 endmodule
+
+
+
+
+    
+
