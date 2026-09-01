7333279d34410007cc942c1da6caabfcc3763779
JCARNEIROX <joao997gomes@gmail.com>
2026-08-31 21:50:58 -0300

    Run com Read_Latency = 2

---
+++ Phase2/SD242_Verificaçao de Sistemas Digitais/Lab2_UVM/xcelium.d/run.lnx8664.23.09.d/.timestamp.ts
@@ -0,0 +1,5 @@
+1788221717 /prj/ci/workarea/aluno3/ProjetosCarneiro/CI_Digital/Phase2/SD242_Verificaçao de Sistemas Digitais/Lab2_UVM/tb_params_pkg.sv
+1788221717 /prj/ci/workarea/aluno3/ProjetosCarneiro/CI_Digital/Phase2/SD242_Verificaçao de Sistemas Digitais/Lab2_UVM/interface.sv
+1788221717 /prj/ci/workarea/aluno3/ProjetosCarneiro/CI_Digital/Phase2/SD242_Verificaçao de Sistemas Digitais/Lab2_UVM/generic_if.sv
+1788221717 /prj/ci/workarea/aluno3/ProjetosCarneiro/CI_Digital/Phase2/SD242_Verificaçao de Sistemas Digitais/Lab2_UVM/top_tb.sv
+1788221717 /prj/ci/workarea/aluno3/ProjetosCarneiro/CI_Digital/Phase2/SD242_Verificaçao de Sistemas Digitais/Lab2_UVM/add_pkg.sv
